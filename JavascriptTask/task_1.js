655841e174f617851c6bd3f4f53bf7dccb5bf1b5
GulsenZalova <zalova0202@gmail.com>
2022-03-15 21:31:14 +0400

    task_1 h'll edildi

--- JavascriptTask/task_1.js
+++ JavascriptTask/task_1.js
@@ -1,1 +1,10 @@
 // a b və c ədədləri verilir. a və b ədədlərindən hansının c ədədinə daha yaxın olduğunu tapmağınız tələb olunur.
+let a=prompt("Zəhmət olmasa a ədədini daxil edin: ");
+let b=prompt("Zəhmət olmasa b ədədini daxil edin: ");
+let c=prompt("Zəhmət olmasa c ədədini daxil edin: ");
+
+if ((Number(c)-Number(a))>(Number(c)-Number(b))){
+    alert(" Daxil edilən b ədədi c ədədinə daha yaxındır. ")
+}else{
+    alert(" Daxil edilən a ədədi c ədədinə daha yaxındır. ")
+}
