49769e3806eecb167a8918c5950df4cbf1e450f3
GulsenZalova <zalova0202@gmail.com>
2022-03-15 23:28:09 +0400

    update

--- JavascriptTask/task_1.js
+++ JavascriptTask/task_1.js
@@ -3,8 +3,10 @@
 let a=prompt("Zəhmət olmasa a ədədini daxil edin: ");
 let b=prompt("Zəhmət olmasa b ədədini daxil edin: ");
 let c=prompt("Zəhmət olmasa c ədədini daxil edin: ");
+let c_a=(Number(c)-Number(a));
+let c_b=(Number(c)-Number(b));
 
-if ((Number(c)-Number(a))>(Number(c)-Number(b))){
+if (c_a > c_b){
     alert(" Daxil edilən b ədədi c ədədinə daha yaxındır. ")
 }else{
     alert(" Daxil edilən a ədədi c ədədinə daha yaxındır. ")
