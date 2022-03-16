ac232a785751652b26c7bc9346231b28d37feb11
GulsenZalova <zalova0202@gmail.com>
2022-03-16 22:35:05 +0400

    folder adı dəyişdirildi

---
+++ JsFirstSteps/task_3.js
@@ -0,0 +1,6 @@
+// verilən x ədədinin rəqəmlərini ekrana çap edən proqram yazın.
+
+// let eded=prompt("Zəhmət olmasa ədəd daxil edin: ");
+// for(let i=0; i<eded.length; i++){
+//     console.log(eded.charAt(i))
+// }
---
+++ JsFirstSteps/task_4.js
@@ -0,0 +1,5 @@
+// verilən 4 ədədi böyükdən kiçiyə doğru ekrana çap edən program yazın.
+
+// let azalanSira=[3, 13, 23, 33]
+// azalanSira.sort(function(a,b){return(b-a)})
+// document.write(azalanSira)
