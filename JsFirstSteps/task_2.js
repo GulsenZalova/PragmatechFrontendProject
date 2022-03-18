60051ffaef6fa0c534fd234260e4a2f3de76f693
GulsenZalova <zalova0202@gmail.com>
2022-03-18 04:28:53 +0400

    task_2 update

--- JsFirstSteps/task_2.js
+++ JsFirstSteps/task_2.js
@@ -1,6 +1,10 @@
 // x adında dəyişən təyin edin. bu dəyişənə istənilən tipdə data təyin edilə bilər.Sizdən tələb olununan daxil edilən dəyərin növünü tapmaq və ona uyğun ekrana mesaj yazdırmaqdır. (Nümunə: Daxil etdiyini dəyər rəqəmdir).
 
-// let x=A4;
-// let tip=typeof x;
-// alert(`Sizin daxil etdiyiniz dəyərin tipi: ${tip}`)
 
+let a=prompt("Dəyər  daxil edin: ");
+
+if(a*1==a){
+    alert(`Daxil etdiyiniz ${a} dəyəri ədəddir.`)
+}else if(a*1!==1){
+    alert(`Daxil etdiyiniz ${a} dəyəri stringdir.`)
+}
