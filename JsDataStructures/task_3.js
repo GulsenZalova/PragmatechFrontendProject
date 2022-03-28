08b5747aed232c52e1b087d35b6c998b03fb8a87
GulsenZalova <zalova0202@gmail.com>
2022-03-29 02:42:44 +0400

    task_4 həll edildi, loop istifadə etməmişəm

--- JsDataStructures/task_3.js
+++ JsDataStructures/task_3.js
@@ -1,14 +1,15 @@
-// let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
-// let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
-// let ages = [23, 34, 40, 58, 60];
-// let students = {
-//     adlar: names,
-//     soyadlar: surnames,
-//     yaslar: ages
-// }
+let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
+let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
+let ages = [23, 34, 40, 58, 60];
+let students = {
+    adlar: names,
+    soyadlar: surnames,
+    yaslar: ages
+}
 
 //expected output
 // Ad: Eli,Soyad:Piriyev,Yas:23
 // Ad: Ehmed,Soyad:Memmedov,Yas:34
 
-// console.log("Ad: " + students["adlar"].names[0] + ", Soyad: " + students[1][1] +", Yaş: " + students[2][1] )
+console.log("Ad: " + students["adlar"][0] + ", Soyad: " + students["soyadlar"][0] +", Yaş: " + students["yaslar"][0] )
+console.log("Ad: " + students["adlar"][1] + ", Soyad: " + students["soyadlar"][1] +", Yaş: " + students["yaslar"][1] )
--- JsDataStructures/task_4.js
+++ JsDataStructures/task_4.js
@@ -1,30 +1,30 @@
 // inputs
 
-let students = [{
-    ad: 'Eli',
-    soyad: 'Piriyev',
-    yas: 23
-}, {
-    ad: 'Ehmed',
-    soyad: 'Memmedov',
-    yas: 34
-}, {
-    ad: 'Sara',
-    soyad: 'Qurbanova',
-    yas: 40
-}, {
-    ad: 'Sevda',
-    soyad: 'Ezizova',
-    yas: 58
-}, {
-    ad: 'Qurban',
-    soyad: 'Sadiqzade',
-    yas: 60
-}]
+// let students = [{
+//     ad: 'Eli',
+//     soyad: 'Piriyev',
+//     yas: 23
+// }, {
+//     ad: 'Ehmed',
+//     soyad: 'Memmedov',
+//     yas: 34
+// }, {
+//     ad: 'Sara',
+//     soyad: 'Qurbanova',
+//     yas: 40
+// }, {
+//     ad: 'Sevda',
+//     soyad: 'Ezizova',
+//     yas: 58
+// }, {
+//     ad: 'Qurban',
+//     soyad: 'Sadiqzade',
+//     yas: 60
+// }]
 
 //expected output
 // Ad: Eli,Soyad:Piriyev,Yas:23
 // Ad: Ehmed,Soyad:Memmedov,Yas:34
 
-console.log("Ad: " + students[0]["ad"] + ", Soyad: " + students[0]["soyad"] +", Yaş: " + students[0]["yas"] )
-console.log("Ad: " + students[1]["ad"] + ", Soyad: " + students[1]["soyad"] +", Yaş: " + students[1]["yas"] )
+// console.log("Ad: " + students[0]["ad"] + ", Soyad: " + students[0]["soyad"] +", Yaş: " + students[0]["yas"] )
+// console.log("Ad: " + students[1]["ad"] + ", Soyad: " + students[1]["soyad"] +", Yaş: " + students[1]["yas"] )
