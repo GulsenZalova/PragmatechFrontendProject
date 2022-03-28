0ac099f8547e233f214a30a87ea4ce53abdcc114
GulsenZalova <zalova0202@gmail.com>
2022-03-29 02:05:20 +0400

    task_1 hazır, loop ilə yazmamışam

---
+++ JsDataStructures/task_1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// (function() {
+//  let i=0;
+//  i++;
+//  console.log(i);
+//  if(i<100){   
+//  }
+// })()
+
+
+
+
+
+
+
+
+
+
+  // sadəcə burda kod yazmağınıza icaze verilir
+    // teleb olunan 1-100 arasındakı ədədlərin ekrana cap edilməsini təmin etməkdir
+    // while for və digər loop expressionları istifadə etmək olmaz
