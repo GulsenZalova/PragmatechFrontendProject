33ad7a74baf1c698253ecff337bb56d7279fa738
GulsenZalova <zalova0202@gmail.com>
2022-04-01 01:27:32 +0400

    update

--- JsDataStructures/task_1.js
+++ JsDataStructures/task_1.js
@@ -1,15 +1,28 @@
 // "use strict";
 
-// (function() {
-//  let i=1;
-//  if(i<100){ 
-//   i++;
-//   console.log(i);
-//   return i
-//  }
-//  console.log(arguments.callee(i))
-// })()
-
   // sadəcə burda kod yazmağınıza icaze verilir
     // teleb olunan 1-100 arasındakı ədədlərin ekrana cap edilməsini təmin etməkdir
     // while for və digər loop expressionları istifadə etmək olmaz
+
+//  1-ci üsul
+// (function() {
+//  let i=0;
+//  i++;
+//  console.log(i)
+//  if(i<=100){ 
+//  arguments.callee(i)
+//  }
+// })()
+
+// 2-ci üsul
+// (function() {
+//   let i=0;
+//   let sirala=function(){
+//     i++;
+//     console.log(i)
+//     if(i<100){
+//       sirala()
+//     }
+//   }
+//   sirala()
+//  })()
