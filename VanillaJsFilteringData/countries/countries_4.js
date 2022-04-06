b814575eb4ccab51c9cdb88bc654207f3597d1fe
GulsenZalova <zalova0202@gmail.com>
2022-04-07 02:44:57 +0400

    countries task4 qismen hazirlandi

--- VanillaJsFilteringData/countries/countries_4.js
+++ VanillaJsFilteringData/countries/countries_4.js
@@ -1,46 +1,59 @@
 // Ölkə adı ən uzun olan ölkənin şəhərlərini return edən funksiya yazın.
-// countries = {
-//     Afghanistan: [
-//         "Herat",
-//         "Kabul",
-//         "Kandahar",
+countries = {
+    Afghanistan: [
+        "Herat",
+        "Kabul",
+        "Kandahar",
 
-//     ],
-//     BAlbania: [
-//         "Elbasan",
-//         "Petran",
-//         "Pogradec",
-//         "Shkoder",
+    ],
+    BAlbania: [
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
         
-//     ],
-//     SAlgeria: [
-//         "Algiers",
-//         "Annaba",
-//         "Azazga",
-//         "Batna City",
-//         "Tlemcen"
-//     ],
-//     ZAndorra: [
-//         "Andorra la Vella",
-//         "Canillo",
-//         "Encamp",
-//         "Engordany",
-//         "Escaldes-Engordany",
-//         "La Massana",
-//         "Llorts",
-//         "Ordino",
-//         "Santa Coloma",
-//         "Sispony",
-//         "Soldeu"
-//     ],
-//     XAngola: [
-//         "Ambriz",
-//         "Benguela",
-//         "Cabinda",
-//         "Cacole",
-//         "Camabatela",
-//         "Cazeta",
-//         "Huambo",
+    ],
+    SAlgeria: [
+        "Algiers",
+        "Annaba",
+        "Azazga",
+        "Batna City",
+        "Tlemcen"
+    ],
+    ZAndorra: [
+        "Andorra la Vella",
+        "Canillo",
+        "Encamp",
+        "Engordany",
+        "Escaldes-Engordany",
+        "La Massana",
+        "Llorts",
+        "Ordino",
+        "Santa Coloma",
+        "Sispony",
+        "Soldeu"
+    ],
+    XAngola: [
+        "Ambriz",
+        "Benguela",
+        "Cabinda",
+        "Cacole",
+        "Camabatela",
+        "Cazeta",
+        "Huambo",
     
-//     ],
-// }
+    ],
+}
+
+function countryName(){
+    let country=Object.keys(countries)
+    for(let i=0;i<country.length; i++){
+     let name=Object.entries(countries)
+     console.log(country[i].length)
+    if(country[i].length==11){
+        let showName=(name[i][1])
+        alert(showName)
+    }
+    }
+}
+countryName()
