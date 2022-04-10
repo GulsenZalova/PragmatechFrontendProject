a8b8f754e3ee1ff7ad327fdb7fc445c1723573e5
GulsenZalova <zalova0202@gmail.com>
2022-04-10 16:25:18 +0400

    countries task 5-də səhv düzəldirldi

--- VanillaJsFilteringData/countries/countries_2.js
+++ VanillaJsFilteringData/countries/countries_2.js
@@ -1,49 +1,49 @@
 "use strict";
 // Hərfə görə ölkə sayının olduğu object return edən funksiya yazın. Nümunə {"A":20,"B":15}
-// let countries = {
-//     Afghanistan: [
-//         "Herat",
-//         "Kabul",
-//         "Kandahar",
+let countries = {
+    Afghanistan: [
+        "Herat",
+        "Kabul",
+        "Kandahar",
 
-//     ],
-//     BAlbania: [
-//         "Elbasan",
-//         "Petran",
-//         "Pogradec",
-//         "Shkoder",
+    ],
+    BAlbania: [
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
         
-//     ],
-//     SAlgeria: [
-//         "Algiers",
-//         "Annaba",
-//         "Azazga",
-//         "Batna City",
-//         "Tlemcen"
-//     ],
-//     ZAndorra: [
-//         "Andorra la Vella",
-//         "Canillo",
-//         "Encamp",
-//         "Engordany",
-//         "Escaldes-Engordany",
-//         "La Massana",
-//         "Llorts",
-//         "Ordino",
-//         "Santa Coloma",
-//         "Sispony",
-//         "Soldeu"
-//     ],
-//     XAngola: [
-//         "Ambriz",
-//         "Benguela",
-//         "Cabinda",
-//         "Cacole",
-//         "Camabatela",
-//         "Cazeta",
-//         "Huambo",
+    ],
+    SAlgeria: [
+        "Algiers",
+        "Annaba",
+        "Azazga",
+        "Batna City",
+        "Tlemcen"
+    ],
+    ZAndorra: [
+        "Andorra la Vella",
+        "Canillo",
+        "Encamp",
+        "Engordany",
+        "Escaldes-Engordany",
+        "La Massana",
+        "Llorts",
+        "Ordino",
+        "Santa Coloma",
+        "Sispony",
+        "Soldeu"
+    ],
+    XAngola: [
+        "Ambriz",
+        "Benguela",
+        "Cabinda",
+        "Cacole",
+        "Camabatela",
+        "Cazeta",
+        "Huambo",
     
-//     ],
-// }
+    ],
+}
 
 
--- VanillaJsFilteringData/countries/countries_5.js
+++ VanillaJsFilteringData/countries/countries_5.js
@@ -1,57 +1,60 @@
 "use strict";
 // Bütün ölkələrin şəhər saylarının olduğu objecti return edən funksiya yazın .Nümunə {"Azerbaijan":70,"Turkey":300}
-// countries = {
-//     Afghanistan: [
-//         "Herat",
-//         "Kabul",
-//         "Kandahar",
+countries = {
+    Afghanistan: [
+        "Herat",
+        "Kabul",
+        "Kandahar",
 
-//     ],
-//     BAlbania: [
-//         "Elbasan",
-//         "Petran",
-//         "Pogradec",
-//         "Shkoder",
+    ],
+    BAlbania: [
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
         
-//     ],
-//     SAlgeria: [
-//         "Algiers",
-//         "Annaba",
-//         "Azazga",
-//         "Batna City",
-//         "Tlemcen"
-//     ],
-//     ZAndorra: [
-//         "Andorra la Vella",
-//         "Canillo",
-//         "Encamp",
-//         "Engordany",
-//         "Escaldes-Engordany",
-//         "La Massana",
-//         "Llorts",
-//         "Ordino",
-//         "Santa Coloma",
-//         "Sispony",
-//         "Soldeu"
-//     ],
-//     XAngola: [
-//         "Ambriz",
-//         "Benguela",
-//         "Cabinda",
-//         "Cacole",
-//         "Camabatela",
-//         "Cazeta",
-//         "Huambo",
+    ],
+    SAlgeria: [
+        "Algiers",
+        "Annaba",
+        "Azazga",
+        "Batna City",
+        "Tlemcen"
+    ],
+    ZAndorra: [
+        "Andorra la Vella",
+        "Canillo",
+        "Encamp",
+        "Engordany",
+        "Escaldes-Engordany",
+        "La Massana",
+        "Llorts",
+        "Ordino",
+        "Santa Coloma",
+        "Sispony",
+        "Soldeu"
+    ],
+    XAngola: [
+        "Ambriz",
+        "Benguela",
+        "Cabinda",
+        "Cacole",
+        "Camabatela",
+        "Cazeta",
+        "Huambo",
     
-//     ],
-// }
+    ],
+}
 
-// function countryAndCity(){
-//     let keys=Object.keys(countries);
-//     let vals=Object.values(countries)
-//     for(let i=0;i<keys.length;i++){
-//     let show=(`"${keys[i]}":${vals[i].length}`)
-//     console.log(show);
-//     }
-// }
-// countryAndCity()
+function countryAndCity(){
+    let showObj={}
+    let keys=Object.keys(countries);
+    let vals=Object.values(countries)
+    for(let i=0; i < vals.length; i++){
+    showObj[`"${keys[i]}"`]=vals[i].length
+    }
+    return console.log(showObj);
+}
+countryAndCity()
+
+
