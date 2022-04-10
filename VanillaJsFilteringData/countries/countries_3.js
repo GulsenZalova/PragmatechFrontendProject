70dbf4a60b325860eadfd921c8154afe8eed9d3d
GulsenZalova <zalova0202@gmail.com>
2022-04-10 20:41:01 +0400

    countries task3-də səhv düzəldildi

--- VanillaJsFilteringData/countries/countries_3.js
+++ VanillaJsFilteringData/countries/countries_3.js
@@ -1,48 +1,62 @@
 "use strict";
 // Ən çox şəhəri olan ölkə adını return edən funksiya yazın
-// countries = {
-//     Afghanistan: [
-//         "Herat",
-//         "Kabul",
-//         "Kandahar",
+let countries = {
+    Afghanistan: [
+        "Herat",
+        "Kabul",
+        "Kandahar",
 
-//     ],
-//     BAlbania: [
-//         "Elbasan",
-//         "Petran",
-//         "Pogradec",
-//         "Shkoder",
+    ],
+    BAlbania: [
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
         
-//     ],
-//     SAlgeria: [
-//         "Algiers",
-//         "Annaba",
-//         "Azazga",
-//         "Batna City",
-//         "Tlemcen"
-//     ],
-//     ZAndorra: [
-//         "Andorra la Vella",
-//         "Canillo",
-//         "Encamp",
-//         "Engordany",
-//         "Escaldes-Engordany",
-//         "La Massana",
-//         "Llorts",
-//         "Ordino",
-//         "Santa Coloma",
-//         "Sispony",
-//         "Soldeu"
-//     ],
-//     XAngola: [
-//         "Ambriz",
-//         "Benguela",
-//         "Cabinda",
-//         "Cacole",
-//         "Camabatela",
-//         "Cazeta",
-//         "Huambo",
+    ],
+    SAlgeria: [
+        "Algiers",
+        "Annaba",
+        "Azazga",
+        "Batna City",
+        "Tlemcen"
+    ],
+    ZAndorra: [
+        "Andorra la Vella",
+        "Canillo",
+        "Encamp",
+        "Engordany",
+        "Escaldes-Engordany",
+        "La Massana",
+        "Llorts",
+        "Ordino",
+        "Santa Coloma",
+        "Sispony",
+        "Soldeu"
+    ],
+    XAngola: [
+        "Ambriz",
+        "Benguela",
+        "Cabinda",
+        "Cacole",
+        "Camabatela",
+        "Cazeta",
+        "Huambo",
     
-//     ],
-// }
+    ],
+}
 
+function countryCity(){
+    let vals=Object.values(countries);
+    let all=Object.entries(countries);
+    let citylist=0;
+    let countryName;
+    for(let i=0; i< all.length; i++){
+        if(vals[i].length>citylist){
+            citylist=vals[i].length
+            countryName=all[i][0]
+        }
+    }
+    return console.log(countryName)
+}
+countryCity()
--- VanillaJsFilteringData/countries/countries_4.js
+++ VanillaJsFilteringData/countries/countries_4.js
@@ -1,67 +1,67 @@
 "use strict";
 // Ölkə adı ən uzun olan ölkənin şəhərlərini return edən funksiya yazın.
-countries = {
-    Afghanistan: [
-        "Herat",
-        "Kabul",
-        "Kandahar",
+// countries = {
+//     Afghanistan: [
+//         "Herat",
+//         "Kabul",
+//         "Kandahar",
 
-    ],
-    BAlbania: [
-        "Elbasan",
-        "Petran",
-        "Pogradec",
-        "Shkoder",
+//     ],
+//     BAlbania: [
+//         "Elbasan",
+//         "Petran",
+//         "Pogradec",
+//         "Shkoder",
         
-    ],
-    SAlgeria: [
-        "Algiers",
-        "Annaba",
-        "Azazga",
-        "Batna City",
-        "Tlemcen"
-    ],
-    ZAndorrasdsdfff: [
-        "Andorra la Vella",
-        "Canillo",
-        "Encamp",
-        "Engordany",
-        "Escaldes-Engordany",
-        "La Massana",
-        "Llorts",
-        "Ordino",
-        "Santa Coloma",
-        "Sispony",
-        "Soldeu",
-    ],
-    XAngolabdsxkrishüeofjsdshhu: [
-        "Ambriz",
-        "Benguela",
-        "Cabinda",
-        "Cacole",
-        "Camabatela",
-        "Cazeta",
-        "Huambo",
+//     ],
+//     SAlgeria: [
+//         "Algiers",
+//         "Annaba",
+//         "Azazga",
+//         "Batna City",
+//         "Tlemcen"
+//     ],
+//     ZAndorrasdsdfff: [
+//         "Andorra la Vella",
+//         "Canillo",
+//         "Encamp",
+//         "Engordany",
+//         "Escaldes-Engordany",
+//         "La Massana",
+//         "Llorts",
+//         "Ordino",
+//         "Santa Coloma",
+//         "Sispony",
+//         "Soldeu",
+//     ],
+//     XAngolabdsxkrishüeofjsdshhu: [
+//         "Ambriz",
+//         "Benguela",
+//         "Cabinda",
+//         "Cacole",
+//         "Camabatela",
+//         "Cazeta",
+//         "Huambo",
     
-    ],
-}
+//     ],
+// }
 
 
-function countryLongName(){
-    let keys=Object.keys(countries);
-    let vals=Object.values(countries);
-    let all=Object.entries(countries)
-    let longName=0;
-    let maxValue;
-    let showCity;
-    for(let i=0;i<all.length; i++){
-        if(all[i][0].length > longName){
-            longName=all[i][0].length;
-            maxValue=all[i][0]
-            showCity=all[i][1]
-        }
+// function countryLongName(){
+//     let keys=Object.keys(countries);
+//     let vals=Object.values(countries);
+//     let all=Object.entries(countries)
+//     let longName=0;
+//     let maxValue;
+//     let showCity;
+//     for(let i=0;i<all.length; i++){
+//         if(all[i][0].length > longName){
+//             longName=all[i][0].length;
+//             maxValue=all[i][0]
+//             showCity=all[i][1]
+//         }
         
-    }
-    return console.log(showCity.toString())
-}
-countryLongName()
+//     }
+//     return console.log(showCity.toString())
+// }
+// countryLongName()
