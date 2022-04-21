9ce0db67e6a584e96c527accc9cefca39ea795c7
GulsenZalova <zalova0202@gmail.com>
2022-04-22 00:48:58 +0400

    tapşırıq01

--- DOMCircles/Tapşırıq01/app.js
+++ DOMCircles/Tapşırıq01/app.js
@@ -1,3 +1,4 @@
+"use strict"
 
     // Verilən parametrlərə əsasən dairə yaradan metod yazın
     // _selector -> yaradılan dairənin html daxilində hansı elementin daxilinə əlavə ediləcəyini müəyyən edəcək.Yəni '.box' argumenti daxil edildiyi zaman yaranan dairə .box adlı elementin daxilinə əlavə ediləcək
@@ -5,6 +6,18 @@
     // _backgroundColor->yaradılacaq dairənin arxafon rəngi daxil ediləcək string olaraq
 
 
-//   function createCircle(_selector, _radius, _backgroundColor) {
-  
-//   }
+  function createCircle(_selector, _radius, _backgroundColor) {
+    let container=document.querySelector(`${_selector}`);
+    let circle=document.createElement("div");
+    circle.className="box"
+    container.appendChild(circle)
+    circle.style.width=`${_radius}px`;
+    circle.style.height=`${_radius}px`;
+    circle.style.borderRadius="50%";
+    circle.style.backgroundColor=`${_backgroundColor}`;
+    circle.style.border="1px solid black"
+  }
+  createCircle(".container", 300, "rgb(220, 227, 230")
+
+
+
