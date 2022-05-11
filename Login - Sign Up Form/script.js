ce8a863a6a75a6b82cf47cb7662cf05e6e40b601
GulsenZalova <zalova0202@gmail.com>
2022-05-11 07:12:53 +0400

    password və iconun show-hide məsələsi js ilə  həll edildi

--- Login - Sign Up Form/script.js
+++ Login - Sign Up Form/script.js
@@ -1,1 +1,14 @@
 "use strict";
+// Eye iconuna klik etdikdə passwordun və iconun görünür-gizli olması
+const eyeicon=document.querySelector(".eyeicon");
+const input=document.querySelector("#password");
+
+eyeicon.addEventListener("click", function(){
+   if(input.type === "password"){
+      input.type="text",
+      eyeicon.classList.replace("fa-eye-slash", "fa-eye")
+   }else if(input.type === "text"){
+    input.type="password",
+    eyeicon.classList.replace("fa-eye", "fa-eye-slash")
+   }
+})
